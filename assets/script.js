abca71dd37b8535db833983253deb97b5f1a4e02
Isaac Galvan <isaacgdev@gmail.com>
2021-11-27 16:26:10 -0600

    adding functionality for starting quiz, adding question and answer buttons, and setting styles

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,21 @@
 let quizBank = [
   {
     question: 'Which built-in method returns the string representation of the number\'s value?',
-    options: {
-      a: 'toValue()',
-      b: 'toNumber()',
-      c: 'toString()',
-      d: 'None of the above.'
-    },
-    answer: 'c',
-    explanation: 'toString() method returns the string representation of the number\'s value.'
+    options: [
+      { text: 'toValue()', correct: false },
+      { text: 'toNumber()', correct: false },
+      { text: 'toString()', correct: true },
+      { text: 'None of the above.', correct: false }
+    ]
   },
   {
     question: 'The "function" and " var" are known as:',
-    options: {
-      a: 'Keywords',
-      b: 'Data types',
-      c: 'Declaration statements',
-      d: 'Prototypes'
-    },
-    answer: 'c',
-    explanation: 'The "function" and "var" both are the Declaration statements. These both are used for defining, and declaring variable, function in anywhere in the program.'
+    options: [
+      { text: 'Keywords', correct: false },
+      { text: 'Data types', correct: false },
+      { text: 'Declaration statements', correct: true },
+      { text: 'Prototypes', correct: false}
+    ]
   },
   {
     question: 'Which of the following variables takes precedence over the others if the names are the same?',
@@ -59,23 +55,76 @@ let quizBank = [
 
 let homePage = document.getElementById('home');
 let quizGame = document.getElementById('quiz');
+let feedbackWrapper = document.getElementById('feedback-wrapper');
+let endScreen = document.getElementById('end-screen');
 let startQuiz = document.getElementById('start');
-let quizQuestion = document.getElementById('question');
-let optionA = document.getElementById('option-a');
-let optionB = document.getElementById('option-b');
-let optionC = document.getElementById('option-c');
-let optionD = document.getElementById('option-d');
+let questionEl = document.getElementById('question');
+let answerBtnEl = document.getElementById('btn-options');
+let countdownTimer = document.getElementById('countdown-timer');
+let totalScore = document.getElementById('score');
+let feedbackEl = document.getElementById('feedback');
+let questionIndex = 0;
+let count = 100;
 
-// Hides quiz on page load
+// Hides quiz and end screen on page load
 quizGame.style.display = 'none';
+endScreen.style.display = 'none';
+feedbackWrapper.style.display = 'none';
 
+// Start game on click
 startQuiz.addEventListener('click', function() {
   quizGame.style.display = 'block';
   homePage.style.display = 'none';
+  buildQuiz();
+  countdown();
 });
 
 function buildQuiz() {
+  showQuestion();  
 
+  console.log("built")
 };
 
-// console.log(quizBank)
+function showQuestion() {
+  questionEl.innerText = quizBank[questionIndex].question;
+
+  quizBank[questionIndex].options.forEach(option => {
+    const answerBtn = document.createElement('button');
+    answerBtn.innerText = option.text;
+    answerBtn.classList.add('btn');
+    if (option.correct) {
+      answerBtn.dataset.correct = option.correct;
+    };
+    answerBtn.addEventListener('click', selectAnswer);
+    answerBtnEl.appendChild(answerBtn);
+  })
+};
+
+function selectAnswer(event) {
+  let selectedBtn = event.target;
+  let correct = selectedBtn.dataset.correct;
+  feedbackWrapper.style.display = 'block';
+
+  if (correct) {
+    feedbackEl.innerText = 'Correct!';
+  } else {
+    feedbackEl.innerText = 'Wrong!';
+    count = count - 5;
+  };
+};
+
+// Countdown timer
+function countdown() {
+  timer = setInterval(function() {
+    countdownTimer.textContent = count--;
+    if(count == 0) {
+      clearInterval(timer);
+      quizGame.style.display = 'none';
+      endScreen.style.display = 'block';
+      totalScore.textContent = count;
+      countdownTimer.textContent = count--;
+    } 
+}, 1000);
+};
+
+console.log(quizBank[questionIndex].options)
